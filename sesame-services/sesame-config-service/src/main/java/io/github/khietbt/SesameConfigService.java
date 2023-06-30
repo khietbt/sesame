f98ec9506e6ef1842d90f39a60a732374768ae81
khietbt <khietbt@clevai.edu.vn>
2023-06-30 02:36:28 +0000

    Add lombok

--- sesame-services/sesame-config-service/src/main/java/io/github/khietbt/SesameConfigService.java
+++ sesame-services/sesame-config-service/src/main/java/io/github/khietbt/SesameConfigService.java
@@ -1,15 +1,26 @@
 package io.github.khietbt;
 
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SesameConfigService {
+@Slf4j
+public class SesameConfigService implements CommandLineRunner {
+
+  @Value("${spring.cloud.config.server.native.searchLocations}")
+  private String searchLocations;
 
   public static void main(String[] args) {
     SpringApplication.run(SesameConfigService.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info("Woke up successfully with search locations = {}", searchLocations);
   }
 }
