ebc0407194eb2f2f6f9acc74ee51ea687cfb3225
khietbt <khietbt@clevai.edu.vn>
2023-06-30 08:52:34 +0000

    Add sesame-discovery-service

--- sesame-services/sesame-discovery-service/src/main/java/io/github/khietbt/SesameDiscoveryService.java
+++ sesame-services/sesame-discovery-service/src/main/java/io/github/khietbt/SesameDiscoveryService.java
@@ -2,13 +2,13 @@ package io.github.khietbt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
-public class SesameConfigService {
+public class SesameDiscoveryService {
 
   public static void main(String[] args) {
-    SpringApplication.run(SesameConfigService.class, args);
+    SpringApplication.run(SesameDiscoveryService.class, args);
   }
 }
