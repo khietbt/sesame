ea8eb4e7a1d49f84435ff53b765cf8a796c7e2e7
khietbt <khietbt@clevai.edu.vn>
2023-07-17 08:54:51 +0000

    Introduce SesameUserService

---
+++ sesame-services/sesame-user-service/src/main/java/io/github/khietbt/SesameUserService.java
@@ -0,0 +1,12 @@
+package io.github.khietbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesameUserService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SesameUserService.class, args);
+  }
+}
