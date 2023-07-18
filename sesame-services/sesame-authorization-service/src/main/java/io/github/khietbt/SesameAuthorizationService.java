40ee4e2da3c2bdc65733540e20fae3ca1d8d8f71
khietbt <khietbt@clevai.edu.vn>
2023-07-18 10:40:53 +0000

    Add a new service SesameAuthorizationService

---
+++ sesame-services/sesame-authorization-service/src/main/java/io/github/khietbt/SesameAuthorizationService.java
@@ -0,0 +1,12 @@
+package io.github.khietbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesameAuthorizationService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SesameAuthorizationService.class, args);
+  }
+}
