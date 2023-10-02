1f15e579e136e5ad39a032287bc076e962cc77d7
khietbt <khietbt@clevai.edu.vn>
2023-10-02 04:06:29 +0000

    Apply auditing with r2dbc

--- sesame-services/sesame-user-service/src/main/java/io/github/khietbt/callbacks/DefaultBeforeConvertCallback.java
+++ sesame-services/sesame-user-service/src/main/java/io/github/khietbt/callbacks/DefaultBeforeConvertCallback.java
@@ -1,5 +1,6 @@
 package io.github.khietbt.callbacks;
 
+import io.asyncer.r2dbc.mysql.internal.NotNullByDefault;
 import io.github.khietbt.entities.Auditable;
 import java.util.UUID;
 import org.reactivestreams.Publisher;
@@ -7,19 +8,15 @@ import org.springframework.data.r2dbc.mapping.event.BeforeConvertCallback;
 import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 
 @Component
-public class AuditableBeforeSaveCallback<T> implements BeforeConvertCallback<T> {
+public class DefaultBeforeConvertCallback<T  extends Auditable> implements BeforeConvertCallback<T> {
 
   @Override
-  public Publisher<T> onBeforeConvert(T entity, SqlIdentifier table) {
-    if (entity instanceof Auditable auditable) {
-
-      if (auditable.getUuid() == null) {
-        auditable.setUuid(UUID.randomUUID());
-      }
-
-      return Mono.just((T) auditable);
+  public @NonNull Publisher<T> onBeforeConvert(@NonNull T entity, @NonNull SqlIdentifier table) {
+    if (entity.getUuid() == null) {
+      entity.setUuid(UUID.randomUUID());
     }
 
     return Mono.just(entity);
