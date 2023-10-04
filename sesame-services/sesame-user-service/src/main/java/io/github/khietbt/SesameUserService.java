f5451d592067ac19e058df581206c49bead86cd1
khietbt <khietbt@gmail.com>
2023-10-04 11:02:06 +0000

    Clean source code

--- sesame-services/sesame-user-service/src/main/java/io/github/khietbt/SesameUserService.java
+++ sesame-services/sesame-user-service/src/main/java/io/github/khietbt/SesameUserService.java
@@ -1,10 +1,6 @@
 package io.github.khietbt;
 
-import io.github.khietbt.entities.User;
-import io.github.khietbt.repositories.UserRepository;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,24 +11,11 @@ import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 @EnableR2dbcAuditing
 public class SesameUserService implements CommandLineRunner {
 
-  @Autowired
-  private UserRepository userRepository;
-
   public static void main(String[] args) {
     SpringApplication.run(SesameUserService.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    User u = userRepository.save(
-        User
-          .builder()
-          .username(UUID.randomUUID().toString())
-          .fullname("test")
-          .build()
-      )
-      .block();
-
-    log.error("User = {}", u);
   }
 }
