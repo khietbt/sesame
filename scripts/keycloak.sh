8d9e1114ba2eb1216b0df87a5977fd71f15a1c0c
khietbt <khietbt@gmail.com>
2023-10-19 07:05:41 +0000

    Add scripts to start / stop keycloak via docker-compose

--- scripts/keycloak.sh
+++ scripts/keycloak.sh
@@ -3,3 +3,8 @@
 TOP_DIR=$(dirname "$(cd -- "$(dirname "${0}")" >/dev/null 2>&1 && pwd -P)")
 
 source "${TOP_DIR}/scripts/libs.sh"
+
+VENDOR_DIR=${TOP_DIR}/vendors
+KEYCLOAK_DIR=${VENDOR_DIR}/keycloak
+
+"${KEYCLOAK_DIR}"/scripts/start.sh
