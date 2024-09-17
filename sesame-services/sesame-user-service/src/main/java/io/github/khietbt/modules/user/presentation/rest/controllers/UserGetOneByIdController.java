abce5685818dae30b3dece1b82b016b86e5babb4
khietbt <khietbt@gmail.com>
2024-09-17 14:41:09 +0700

    Allow keycloak to protect a resource server

--- sesame-services/sesame-user-service/src/main/java/io/github/khietbt/modules/user/presentation/rest/controllers/UserGetOneByIdController.java
+++ sesame-services/sesame-user-service/src/main/java/io/github/khietbt/modules/user/presentation/rest/controllers/UserGetOneByIdController.java
@@ -4,8 +4,10 @@ import io.github.khietbt.modules.user.application.queries.UserGetOneByIdQuery;
 import io.github.khietbt.modules.user.domain.entities.User;
 import io.github.khietbt.modules.user.domain.valueobjects.UserId;
 import io.github.khietbt.modules.user.presentation.rest.responses.UserGetOneByIdResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,19 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@Slf4j
 public class UserGetOneByIdController {
     @Autowired
     private QueryGateway queryGateway;
 
     @GetMapping("/users/{id}")
     public CompletableFuture<?> getOneById(@PathVariable(name = "id") UUID id) {
+
+        var context = SecurityContextHolder.getContext();
+        var authentication = context.getAuthentication();
+
+        log.info("{}", authentication.getAuthorities());
+
         var query = new UserGetOneByIdQuery(new UserId(id));
 
         return queryGateway
