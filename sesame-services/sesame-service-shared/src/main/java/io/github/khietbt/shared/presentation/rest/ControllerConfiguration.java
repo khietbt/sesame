47c6475644aa072a88bfd675aa55ae2ad3e8241a
khietbt <khietbt@gmail.com>
2024-10-02 10:56:16 +0700

    Update UserGetOneById by hateoas

--- sesame-services/sesame-service-shared/src/main/java/io/github/khietbt/shared/presentation/rest/ControllerConfiguration.java
+++ sesame-services/sesame-service-shared/src/main/java/io/github/khietbt/shared/presentation/rest/ControllerConfiguration.java
@@ -4,21 +4,15 @@ import io.github.khietbt.shared.domain.exceptions.ConflictException;
 import io.github.khietbt.shared.domain.exceptions.ExceptionDetails;
 import io.github.khietbt.shared.domain.exceptions.NotFoundException;
 import org.axonframework.commandhandling.CommandExecutionException;
-import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.lang.reflect.Constructor;
 
 @ControllerAdvice
-public class ControllerConfiguration implements ResponseBodyAdvice<Object> {
+public class ControllerConfiguration {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handle(RuntimeException e) {
         var status = HttpStatus.INTERNAL_SERVER_ERROR;
@@ -58,26 +52,5 @@ public class ControllerConfiguration implements ResponseBodyAdvice<Object> {
         }
 
         return e;
-    }
-
-    @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        return true;
-    }
-
-    @Override
-    public Object beforeBodyWrite(
-            Object original,
-            MethodParameter returnType,
-            MediaType selectedContentType,
-            Class<? extends HttpMessageConverter<?>> selectedConverterType,
-            ServerHttpRequest request,
-            ServerHttpResponse response
-    ) {
-        if (!(original instanceof WrapperResponse)) {
-            return WrapperResponse.success(original);
-        }
-
-        return original;
     }
 }
--- sesame-services/sesame-user-service/src/main/java/io/github/khietbt/modules/user/presentation/rest/controllers/UserGetOneByIdController.java
+++ sesame-services/sesame-user-service/src/main/java/io/github/khietbt/modules/user/presentation/rest/controllers/UserGetOneByIdController.java
@@ -7,12 +7,15 @@ import io.github.khietbt.modules.user.presentation.rest.responses.UserGetOneById
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+
 
 @RestController
 @Slf4j
@@ -26,6 +29,14 @@ public class UserGetOneByIdController {
 
         return queryGateway
                 .query(query, User.class)
-                .thenApplyAsync(UserGetOneByIdResponse::fromDomain);
+                .thenApplyAsync(UserGetOneByIdResponse::fromDomain)
+                .thenApplyAsync(EntityModel::of)
+                .thenApplyAsync(
+                        (u) -> u.add(
+                                WebMvcLinkBuilder.linkTo(
+                                        WebMvcLinkBuilder.methodOn(UserGetOneByIdController.class).getOneById(id)
+                                ).withSelfRel()
+                        )
+                );
     }
 }
